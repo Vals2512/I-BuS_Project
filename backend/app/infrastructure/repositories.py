9c1ca73713391629e4ddbac2def08f943a221ac3
Laura Daniela Guevara <laura.guevara02@uptc.edu.co>
2026-05-22 20:05:36 -0500

    SCRUM-68 API de Gestión y CRUD de Rutas

--- backend/app/infrastructure/repositories.py
+++ backend/app/infrastructure/repositories.py
@@ -35,20 +35,60 @@ class RutaRepository:
         
     def get_all(self):
         return self.db.query(RutaDB).all()
-
+        
+    def get_by_id(self, id_ruta: int):
+        return self.db.query(RutaDB).filter(RutaDB.idRuta == id_ruta).first()
+        
+    def create(self, ruta: RutaDB):
+        self.db.add(ruta)
+        self.db.commit()
+        self.db.refresh(ruta)
+        return ruta
+        
+    def update(self, ruta: RutaDB):
+        self.db.commit()
+        self.db.refresh(ruta)
+        return ruta
 class HorarioRepository:
     def __init__(self, db: Session):
         self.db = db
         
     def get_all(self):
         return self.db.query(HorarioDB).all()
-
+        
+    def get_by_id(self, id_horario: int):
+        return self.db.query(HorarioDB).filter(HorarioDB.idHorario == id_horario).first()
+        
+    def create(self, horario: HorarioDB):
+        self.db.add(horario)
+        self.db.commit()
+        self.db.refresh(horario)
+        return horario
+        
+    def update(self, horario: HorarioDB):
+        self.db.commit()
+        self.db.refresh(horario)
+        return horario
 class TiempoRepository:
     def __init__(self, db: Session):
         self.db = db
         
     def get_all(self):
         return self.db.query(TiempoDB).all()
+        
+    def get_by_id(self, id_tiempo: int):
+        return self.db.query(TiempoDB).filter(TiempoDB.idTiempo == id_tiempo).first()
+        
+    def create(self, tiempo: TiempoDB):
+        self.db.add(tiempo)
+        self.db.commit()
+        self.db.refresh(tiempo)
+        return tiempo
+        
+    def update(self, tiempo: TiempoDB):
+        self.db.commit()
+        self.db.refresh(tiempo)
+        return tiempo
 
 class DetalleRutaRepository:
     def __init__(self, db: Session):
