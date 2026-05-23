b36cb6c5a51678ea039b2813a607296bb544bca8
Laura Daniela Guevara <laura.guevara02@uptc.edu.co>
2026-05-22 19:51:10 -0500

    SCRUM-66 listar Barrios y Empresas

--- backend/app/application/use_cases.py
+++ backend/app/application/use_cases.py
@@ -1,6 +1,27 @@
-from app.infrastructure.repositories import BarrioRepository
 from sqlalchemy.orm import Session
+from app.infrastructure.repositories import BarrioRepository, EmpresaRepository
+from app.domain.models import Barrio as DomainBarrio, Empresa as DomainEmpresa
 
 def listar_barrios(db: Session):
     repo = BarrioRepository(db)
-    return repo.get_all()
+    barrios_db = repo.get_all()
+    return [
+        DomainBarrio(id=b.idBarrio, nombre=b.nombreBarrio)
+        for b in barrios_db
+    ]
+
+def listar_empresas(db: Session):
+    repo = EmpresaRepository(db)
+    empresas_db = repo.get_all()
+    return [
+        DomainEmpresa(
+            id=e.idEmpresa,
+            nombreEmpresa=e.nombreEmpresa,
+            anioFundacion=e.anioFundacion,
+            direccion=e.direccion,
+            telefono=e.telefono,
+            cantBuses=e.cantBuses,
+            cantConductores=e.cantConductores
+        )
+        for e in empresas_db
+    ]
